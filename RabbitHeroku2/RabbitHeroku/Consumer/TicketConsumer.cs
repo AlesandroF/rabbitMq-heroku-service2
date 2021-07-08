23e18a3d24d8bc6b9d67770e1274d993066154a6
Alesandro Fidelis <alesandro.fidelis@softplan.com.br>
2021-07-08 18:45:48 -0300

    test

---
+++ RabbitHeroku2/RabbitHeroku/Consumer/TicketConsumer.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using MassTransit;
+using RabbitHeroku;
+
+namespace RabbitHeroku2.Consumer
+{
+    public class TicketConsumer : IConsumer<Ticket>
+    {
+        public async Task Consume(ConsumeContext<Ticket> context)
+        {
+            var data = context.Message;
+        }
+    }
+}
